bbda95702e7611dac471a939f9b87b655725da89
Ram <57409483+authentic-cloud@users.noreply.github.com>
2023-11-15 13:05:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     const gallerySection = document.getElementById("artGallery");
 
-    // List all images from the 'images' folder
-    const imageFolder = "images";
-    const imageExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+    // List all images
+    const images = [
+        "images/artwork1.jpg",
+        "images/artwork2.jpg",
+        "images/artwork3.jpg",
+        "images/artwork4.jpg"
+        "images/artwork5.jpg"
+        "images/artwork6.jpg"
+        
+    ];
 
-    fetchImages(imageFolder, imageExtensions)
-        .then(images => {
-            // Create HTML elements for each image
-            images.forEach(image => {
-                const artworkDiv = document.createElement("div");
-                artworkDiv.classList.add("artwork");
+    // Create HTML elements for each image
+    images.forEach(image => {
+        const artworkDiv = document.createElement("div");
+        artworkDiv.classList.add("artwork");
 
-                const imgElement = document.createElement("img");
-                imgElement.src = image;
-                imgElement.alt = "Artwork";
+        const imgElement = document.createElement("img");
+        imgElement.src = image;
+        imgElement.alt = "Artwork";
 
-                const artistParagraph = document.createElement("p");
-                artistParagraph.textContent = "Artist: Unknown"; // You can replace this with actual artist information
+        const artistParagraph = document.createElement("p");
+        artistParagraph.textContent = "Artist: Unknown"; // You can replace this with actual artist information
 
-                artworkDiv.appendChild(imgElement);
-                artworkDiv.appendChild(artistParagraph);
+        artworkDiv.appendChild(imgElement);
+        artworkDiv.appendChild(artistParagraph);
 
-                gallerySection.appendChild(artworkDiv);
-            });
-        })
-        .catch(error => {
-            console.error("Error fetching images:", error);
-        });
-
-    // Function to fetch images
-    function fetchImages(folder, extensions) {
-        return new Promise((resolve, reject) => {
-            const images = [];
-            const validExtensions = extensions.map(ext => ext.toLowerCase());
-
-            fetch(`./${folder}`)
-                .then(response => response.text())
-                .then(html => {
-                    const parser = new DOMParser();
-                    const doc = parser.parseFromString(html, "text/html");
-                    const links = Array.from(doc.querySelectorAll("a"))
-                        .map(link => link.getAttribute("href"))
-                        .filter(href => validExtensions.some(ext => href.toLowerCase().endsWith(ext)));
-
-                    links.forEach(link => images.push(`${folder}/${link}`));
-
-                    resolve(images);
-                })
-                .catch(reject);
-        });
-    }
+        gallerySection.appendChild(artworkDiv);
+    });
 });
